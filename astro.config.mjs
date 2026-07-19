290e528571f2cdca4787a43873b3749c1f67b26b
Mohammad Sadegh Rasooli <rasooli.ms@gmail.com>
2026-07-18 23:59:54 -0700

    added g search

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,7 +1,8 @@
 import { defineConfig } from 'astro/config';
+import sitemap from '@astrojs/sitemap';
 
-// https://astro.build/config
 export default defineConfig({
   site: 'https://rasoolims.github.io',
   base: '/delsharm',
+  integrations: [sitemap()],
 });
